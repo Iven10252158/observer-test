a9af9bc306f7ce6760d5d10bf0df9280b5258d28
PeiYu <yupeiq1718@osensetech.com>
2023-11-13 13:44:51 +0800

    feat: update package version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  devtools: true,
+  runtimeConfig: {
+    public: {
+      sdkKey: 'RJZCjllKTtzHMamLlsm0'
+    }
+  },
   modules: [
-    '@unocss/nuxt',
     ['@pinia/nuxt', {
       autoImports: [
         'defineStore',
         ['defineStore', 'definePiniaStore']
       ]
     }],
+    '@unocss/nuxt',
     'nuxt-svgo',
-    '@nuxt/devtools'
+    '@nuxt/devtools',
+    '@vueuse/nuxt'
   ],
+  imports: {
+    dirs: ['stores']
+  },
   css: [
     '@unocss/reset/tailwind.css'
   ]
